b164b743a33c07776d1b2d5e67ffa4d0ce8b39c2
BRUNO <BRUNO6053@DIGITALCOLLEGE.LOCAL>
2024-06-06 20:03:18 -0300

    Colocando o restante da calculadora

--- JS/script.js
+++ JS/script.js
@@ -11,4 +11,53 @@ function soma() {
 
     //console.log(numero1, numero2);
     console.log(resultado);
+
+    //Pegando o elemento HTML e colocando na variavel conteudo DIV
+    let conteudoDIV = document.getElementsByClassName('resultado')[0];
+
+    // Pegando o conteudoDIV acessando a prop innerHTML e inserindo o valor calculo, a variavel
+    conteudoDIV.innerHTML = resultado
 }
+
+function subtrair() {
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+
+    let resultado = numero1 - numero2
+
+    console.log(resultado);
+
+    let conteudoDIV = document.getElementsByClassName('resultado')[0];
+
+    conteudoDIV.innerHTML = resultado
+}
+
+function Multiplicação() {
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+
+    let resultado = numero1 * numero2
+
+    console.log(resultado);
+
+    let conteudoDIV = document.getElementsByClassName('resultado')[0];
+
+    conteudoDIV.innerHTML = resultado
+}
+
+function Divisão() {
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+
+    let resultado = numero1 / numero2
+
+    console.log(resultado);
+
+    let conteudoDIV = document.getElementsByClassName('resultado')[0];
+
+    conteudoDIV.innerHTML = resultado
+}
+
+
+
+
