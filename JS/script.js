23167c094ba80a9d9a018ee31c53db74a3b324a1
BRUNO <BRUNO6053@DIGITALCOLLEGE.LOCAL>
2024-06-06 21:50:06 -0300

    adicionando a função none

--- JS/script.js
+++ JS/script.js
@@ -1,63 +1,38 @@
 //console.log('Script via body'), document.getElementById('n1')
+;
+const numero1 = document.getElementById('n1');
+const numero2 = document.getElementById('n2');
+const conteudoDIV = document.querySelector('.resultado');
 
 function soma() {
-    // Pegar os números do inputs do HTML e converter para Float, pois será possível fazer o cálculo com inteiros e decimais
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-
-    let resultado = numero1 + numero2
-
-
-
-    //console.log(numero1, numero2);
-    console.log(resultado);
-
-    //Pegando o elemento HTML e colocando na variavel conteudo DIV
-    let conteudoDIV = document.getElementsByClassName('resultado')[0];
-
-    // Pegando o conteudoDIV acessando a prop innerHTML e inserindo o valor calculo, a variavel
-    conteudoDIV.innerHTML = resultado
+    let resultado = parseFloat(numero1.value) + parseFloat(numero2.value);
+    exibirResultado(resultado) 
 }
 
 function subtrair() {
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-
-    let resultado = numero1 - numero2
-
-    console.log(resultado);
-
-    let conteudoDIV = document.getElementsByClassName('resultado')[0];
-
-    conteudoDIV.innerHTML = resultado
+    let resultado = parseFloat(numero1.value) - parseFloat(numero2.value);
+    exibirResultado(resultado) 
 }
 
 function Multiplicação() {
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-
-    let resultado = numero1 * numero2
-
-    console.log(resultado);
-
-    let conteudoDIV = document.getElementsByClassName('resultado')[0];
-
-    conteudoDIV.innerHTML = resultado
+    let resultado = parseFloat(numero1.value) * parseFloat(numero2.value);
+    exibirResultado(resultado) 
 }
 
 function Divisão() {
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-
-    let resultado = numero1 / numero2
-
-    console.log(resultado);
-
-    let conteudoDIV = document.getElementsByClassName('resultado')[0];
-
-    conteudoDIV.innerHTML = resultado
+    let resultado = parseFloat(numero1.value) / parseFloat(numero2.value);
+    exibirResultado(resultado) 
 }
 
+function exibirResultado(result) {
+    conteudoDIV.style.display = 'flex'
+    conteudoDIV.innerHTML = 'resultado: ' + result;
+}
+
+function Limpar()  {
+    conteudoDIV.style.display = 'none'
+    conteudoDIV.innerHTML = 'Resultado'
+}
 
 
 
